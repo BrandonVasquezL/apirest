dc0f4d92e341cc80c703e1f9d527b3a65e0b875d
BrandonVasquezL <112491290+BrandonVasquezL@users.noreply.github.com>
2023-06-07 00:37:46 -0600

    Update settings.py

--- rtp_back/rtp_back/settings.py
+++ rtp_back/rtp_back/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -58,9 +58,11 @@ ROOT_URLCONF = 'rtp_back.urls'
 
 CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:5500',
+    'http://real-time-parking.x10.mx'
 )
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
+    'http://real-time-parking.x10.mx'
 ]
 TEMPLATES = [
     {
